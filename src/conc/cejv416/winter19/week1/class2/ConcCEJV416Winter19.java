ea02590cc687e00736d8e5fc9d5f751525bd94ef
Y_BEH <Y_BEH@CCE-FB107-03W.concordia.ca>
2019-01-18 19:33:33 -0500

    Initial Commit

---
+++ src/conc/cejv416/winter19/week1/class2/ConcCEJV416Winter19.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conc.cejv416.winter19.week1.class2;
+
+/**
+ *
+ * @author Y_BEH
+ */
+public class ConcCEJV416Winter19 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
